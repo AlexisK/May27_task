3ef7694b3e936e66890cd4dd355ce139a489cad8
AlexisK <alexisk@jollix.me>
2016-05-27 15:30:28 +0300

    add usecase

---
+++ resources/js/app.js
@@ -0,0 +1,33 @@
+
+function declareInstances() {
+    
+    'Truck,Sedan,Disabled'.split(',').map(function(name) {
+        new Tag(name);
+    });
+    
+    window.parking = new Parking();
+    
+    parking.addSlots('Trucks',10,[TAG.Truck,TAG.Disabled,TAG.Sedan]);
+    parking.addSlots('Disabled',5,[TAG.Sedan]);
+    parking.addSlots('Simple',15,[TAG.Disabled,TAG.Sedan]);
+    
+    window.dummy_Truck = new Vehicle([TAG.Truck]);
+    window.dummy_Sedan = new Vehicle([TAG.Sedan]);
+    window.dummy_Disabled = new Vehicle([TAG.Disabled]);
+    
+}
+
+function mainScenario() {
+    parking.yieldState();
+    
+    parking.register(dummy_Sedan.copy());
+    parking.register(dummy_Sedan.copy());
+    parking.register(dummy_Disabled.copy());
+    parking.register(dummy_Truck.copy());
+    
+    parking.yieldState();
+}
+
+// Start
+declareInstances();
+mainScenario();
