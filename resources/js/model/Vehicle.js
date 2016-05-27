8af3c0d60afe5f222611bf8fbcbccbf3a0b7d761
AlexisK <alexisk@jollix.me>
2016-05-27 17:16:05 +0300

    add vehicle unique id

--- resources/js/model/Vehicle.js
+++ resources/js/model/Vehicle.js
@@ -1,9 +1,19 @@
 
+function toLen(str, len) {
+    return new Array(len-str.length+1).join('0')+str;
+}
+
+window.carIter = {
+    num: 41,
+    get: function() { return toLen((carIter.num+=1).toString(),4); }
+};
+
 function Vehicle(taglist) {
     var self = getSelf(this);
     self.inherit(BaseModel);
     
     self.init = function() {
+        self.id = 'VH-'+carIter.get();
         self.taglist = taglist;
     }
     
