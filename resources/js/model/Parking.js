d1a18a46bfdb2f1e03d3b215af0ad3004a0f0111
AlexisK <alexisk@jollix.me>
2016-05-27 15:59:24 +0300

    add Dummy Models

---
+++ resources/js/model/Parking.js
@@ -0,0 +1,31 @@
+
+function Parking() {
+    var self = getSelf(this);
+    self.inherit(BaseModel);
+    
+    self.init = function() {
+        
+    }
+    
+    self.addSlots = function(name, quantity, tagqueue) {
+        
+    }
+    
+    self.register = function(car) {
+        
+    }
+    
+    self.restoreState = function() {
+        
+    }
+    
+    self.saveState = function() {
+        
+    }
+    
+    self.yieldState = function() {
+        
+    }
+    
+    self.init();
+}
---
+++ resources/js/model/Vehicle.js
@@ -0,0 +1,15 @@
+
+function Vehicle(taglist) {
+    var self = getSelf(this);
+    self.inherit(BaseModel);
+    
+    self.init = function() {
+        
+    }
+    
+    self.copy = function() {
+        
+    }
+    
+    self.init();
+}
