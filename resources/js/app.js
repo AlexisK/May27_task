425b7b76d96e4f2e0f978caac21210518fd62537
AlexisK <alexisk@jollix.me>
2016-05-27 22:28:55 +0300

    continious fixture fill scenario

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,15 +29,39 @@ function mainScenario() {
     document.body.appendChild(view);
 }
 
-function fixtureFillScenario(limit) {
-    limit = limit || 0;
+function fixtureFillScenario(limit, tag_keys) {
+    limit = limit || 1;
     
-    var tag_keys = Object.keys(TAG);
+    tag_keys = tag_keys || Object.keys(TAG);
     
     for ( var i = 0; i < limit; i++ ) {
         var tag = TAG[tag_keys[parseInt(Math.random()*tag_keys.length)]];
         parking.register(new Vehicle([tag]).init());
     }
+}
+
+window.fcfRand = 4;
+function fixtureContiniousFillScenario(limit, tag_keys) {
+    // algorythm always trying to fit in its 'ideal' state - targetQuantity, so it will add or remove corresponding amout of vehicles
+    limit = limit || 1;
+    var targetQuantity = limit - fcfRand;
+    if ( targetQuantity <= 0 ) { console.log('Failed to run continious autofill - need limit of '+(fcfRand+1)+' at least'); return 0; }
+    parking.clear();
+    var quantity = 0;
+    
+    var worker = function() {
+        if ( quantity < targetQuantity ) {
+            fixtureFillScenario(targetQuantity-quantity+Math.floor(Math.random()*fcfRand), tag_keys);
+        } else {
+            var toDel = quantity-targetQuantity+Math.floor(Math.random()*fcfRand);
+            for ( var i = 0; i < toDel; i++ ) {
+                var vkeys = Object.keys(VEHICLE);
+                VEHICLE[vkeys[Math.floor(Math.random()*vkeys.length)]].delete();
+            }
+        }
+        quantity = Object.keys(VEHICLE).length;
+    }
+    setInterval(worker, 200);
 }
 
 function migrate() {// Quite simple
